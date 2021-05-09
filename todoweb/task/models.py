c61fb27268b3f8d68b8c766b0321259723c82d5d
aofwittawat <aofwittawat@gmail.com>
2021-05-09 09:04:19 +0700

    add project

--- todoweb/task/models.py
+++ todoweb/task/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+    project_desc = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.project_name
+
+
+class Alltask(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=100)
+    task_desc = models.TextField(null=True, blank=True)
+    task_status = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}  ({})'.format(self.task_name, self.project.project_name)
--- todoweb/task/views.py
+++ todoweb/task/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Alltask
 
 
 def Home(request):
-    return HttpResponse("Start task")
+    myproject = 'เตรียมตัวไปต่างประเทศ'
 
+    # projectlist = ['ศึกษาเรื่องการเงิน','ศึกษาเรื่องการเขียนโปรแกรม','ฝึกเขียนเวปไซค์','ซื้อรถใหม่']
+    projectlist = Project.objects.all()
+    # ใน projectlist เป็น Query set จะหาค่าข้างในต้องแตกออกมาก่อน
+    for pj in projectlist:
+        task_in_pj = Alltask.objects.filter(project=pj)
+        # ทำให้เป็น dict โดยการ add ทีละตัว
+        pj.alltask = task_in_pj
+        # print(pj.alltask)
+
+    context = {'myproject': myproject, 'projectlist': projectlist}
+    return render(request, 'home.html', context)
+
+
+def Contact(request):
+    return HttpResponse('<h1>TEL : 080-244-5777</h1>')
